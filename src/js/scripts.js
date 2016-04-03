0923024b49dc3e943db941dd794068cd31ca0c4f
sfpgmr <sfpgmr@example.com>
2016-04-04 05:27:36 +0900

    canvasに切り替える前

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,19 +1,58 @@
 "use strict";
 import "babel-polyfill";
 
-var vshaderSrc = `
+// フレームバッファに書き込むシェーダー
+var vshaderFSrc = 
+`precision mediump float;
 attribute vec2 position;
+attribute float color;
+uniform vec2 bufferSize;
+varying float vcolor;
+ 
+void main(void) {
+    vec2 bs = bufferSize - bufferSize /  2.0;
+    bs.y = -bs.y;
+    gl_Position = vec4(position / bs, 0.0,1.0);
+    vcolor = color;
+}
+`;
 
+var fshaderFSrc = 
+`precision mediump float;
+varying float vcolor;
+void main(void){
+ gl_FragColor = vec4(vcolor, 0. , 0. , 1.);
+}
+`;
+
+// パレットエミュレートシェーダー
+var vshaderPSrc = 
+`precision mediump float;
+attribute vec2 position;
+attribute vec2 texture_coord;
+varying vec2 vtexture_coord;
+ 
 void main(void) {
     gl_Position = vec4(position,0.0,1.0);
+    vtexture_coord = texture_coord;
 }
 `;
 
-var fshaderSrc = `
+var fshaderPSrc = 
+`precision mediump float;
+
+uniform sampler2D texture;
+uniform vec4 palette_color[8];
+
+varying vec2 vtexture_coord;
+
 void main(void){
- gl_FragColor = vec4(1.0, 1.0 ,1.0 ,1.0);
+ vec4 sampcolor = texture2D(texture, vtexture_coord);
+ int index = int(sampcolor.r);
+ gl_FragColor = palette_color[index];
 }
 `;
+
 
 window.addEventListener('load',()=>{
   // コンソールの作成
@@ -87,35 +126,100 @@ window.addEventListener('load',()=>{
     return vbo;
   }
   
+  
+  // フレームバッファをオブジェクトとして生成する関数
+  function createFramebuffer(width, height){
+    var frameBuffer = gl.createFramebuffer();
+    
+    gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffer);
+    
+    var depthRenderBuffer = gl.createRenderbuffer();
+    gl.bindRenderbuffer(gl.RENDERBUFFER, depthRenderBuffer);
+    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, width, height);
+    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthRenderBuffer);
+    
+    var fTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, fTexture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, fTexture, 0);
+    
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    
+    return {f : frameBuffer, d : depthRenderBuffer, t : fTexture};
+  }
+  
+	// IBOを生成する関数
+	function createIbo(data){
+		var ibo = gl.createBuffer();
+		
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+		return ibo;
+	}  
+  
+ 	// 板ポリゴン
+	var position = [
+		-1.0,  1.0,
+		 1.0,  1.0,
+		-1.0, -1.0,
+		 1.0, -1.0
+	];
+	var texCoord = [
+		0.0, 0.0,
+		1.0, 0.0,
+		0.0, 1.0,
+		1.0, 1.0
+	];
+	var index = [
+		0, 2, 1,
+		2, 3, 1
+	];
+	var vPosition = createVbo(position);
+	var vTexCoord = createVbo(texCoord);
+	var vVBOList  = [vPosition, vTexCoord];
+	var vIndex    = createIbo(index);
+   
   // canvasを黒でクリア(初期化)する
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.viewport(0,0,virtualWidth,virtualHeight);
   gl.clearDepth(1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  // フレームバッファーに書き込むシェーダー・プログラムの作成
+  let vsF = createShader(vshaderFSrc,gl.VERTEX_SHADER);
+  let fsF = createShader(fshaderFSrc,gl.FRAGMENT_SHADER);
   
-  let vs = createShader(vshaderSrc,gl.VERTEX_SHADER);
-  let fs = createShader(fshaderSrc,gl.FRAGMENT_SHADER);
+  let prgF = createProgram(vsF,fsF);
   
-  let prg = createProgram(vs,fs);
+  let prgFPos = gl.getAttribLocation(prgF,'position');
+  let prgFColor = gl.getAttribLocation(prgF,'color');
   
-  let attLocation = gl.getAttribLocation(prg,'position');
-  
-  let attStride = 2;
+  let attStride = 1;
   
   let vertexPos = [
-    0.0,1.0,
-    1.0,0.0,
-    -1.0,0.0
+    160.0,100.0
   ];
   
+  let color = [
+    7.0
+  ];
+  
+  
   let vbo = createVbo(vertexPos);
+  let vboColor = createVbo(color);
   
   gl.bindBuffer(gl.ARRAY_BUFFER,vbo);
-  gl.enableVertexAttribArray(attLocation);
-  gl.vertexAttribPointer(attLocation, attStride, gl.FLOAT, false, 0, 0);
-  
+  gl.enableVertexAttribArray(prgFPos);
+  gl.vertexAttribPointer(prgFPos, attStride, gl.FLOAT, false, 0, 0);
 
-  
+  gl.bindBuffer(gl.ARRAY_BUFFER,vboColor);
+  gl.enableVertexAttribArray(prgFColor);
+  gl.vertexAttribPointer(prgFColor, attStride, gl.FLOAT, false, 0, 0);
   
   function resize(){
     var cont = document.getElementById('content');
